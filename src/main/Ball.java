289b6acbabad0496fe89255804471ac10b4885c6
mika727272 <149627992+mika727272@users.noreply.github.com>
2024-04-01 15:49:32 +0200

    Update Ball.java

--- src/main/Ball.java
+++ src/main/Ball.java
@@ -14,5 +14,5 @@ public class Ball {
 }
 
 //public Ball(int x, int y, int width, int height, Color color) {
-		super(x, y, width, height, color);
-		setOnScreen(true);
+		//super(x, y, width, height, color);
+		//setOnScreen(true);
